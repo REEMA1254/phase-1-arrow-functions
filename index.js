92c194594113ff87c2ced147f78aaed4e7504921
maryam <Reemaali9048@gmail.com>
2023-09-22 10:54:04 +0300

    complete assigment

---
+++ index.js
@@ -0,0 +1,14 @@
+function divide() {
+    return 2000 / 100;
+  } 
+  const square = (x) => {
+    let (x=2)
+    return 2 * 2;
+};
+
+const add = (a, b) => {  
+   let a = 3
+   let b = 4
+    return 3+ 4;
+
+  };
